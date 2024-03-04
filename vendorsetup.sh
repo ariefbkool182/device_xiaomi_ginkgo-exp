6e89dd5127658a8cc6d59cf1847aa022ffa577c6
arkadia182 <52677240+arkadia182@users.noreply.github.com>
2024-03-04 17:58:59 +0700

    Create vendorsetup.sh
    add OP Dolby

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+echo 'Adding patch to Frameworks av'
+cd frameworks/av
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0002-OMXStore-Import-loading-libstagefrightdolby.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0003-Import-Dolby-Effects-initialization.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0004-Add-support-for-loading-prebuilt-ddp-decoder-lib.patch
+wget https://raw.githubusercontent.com/Astridxx/Patch-Kuntao/lineage-21/frameworks_av/0005-Do-not-allow-DAP-effect-to-be-suspended.patch
+patch -p1 <0002-OMXStore-Import-loading-libstagefrightdolby.patch
+patch -p1 <0003-Import-Dolby-Effects-initialization.patch
+patch -p1 <0004-Add-support-for-loading-prebuilt-ddp-decoder-lib.patch
+patch -p1 <0005-Do-not-allow-DAP-effect-to-be-suspended.patch
+cd ../..
